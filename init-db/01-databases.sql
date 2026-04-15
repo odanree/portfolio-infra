08bbf946df44054e966aa8aaed045d7cc6a489fd
Danh Le <nightseek@yahoo.com>
2026-04-15 00:29:12 -0700

    feat: add sec-finint and jd-classifier services to shared infra

--- init-db/01-databases.sql
+++ init-db/01-databases.sql
@@ -6,11 +6,15 @@ CREATE DATABASE compensation_ingest;
 CREATE DATABASE compensation_explorer;
 CREATE DATABASE order_exception_agent;
 CREATE DATABASE inventory_discrepancy;
+CREATE DATABASE sec_financial_intelligence;
+CREATE DATABASE jd_role_classifier;
 
-GRANT ALL PRIVILEGES ON DATABASE compensation_ingest    TO portfolio_user;
-GRANT ALL PRIVILEGES ON DATABASE compensation_explorer   TO portfolio_user;
-GRANT ALL PRIVILEGES ON DATABASE order_exception_agent   TO portfolio_user;
-GRANT ALL PRIVILEGES ON DATABASE inventory_discrepancy   TO portfolio_user;
+GRANT ALL PRIVILEGES ON DATABASE compensation_ingest          TO portfolio_user;
+GRANT ALL PRIVILEGES ON DATABASE compensation_explorer         TO portfolio_user;
+GRANT ALL PRIVILEGES ON DATABASE order_exception_agent         TO portfolio_user;
+GRANT ALL PRIVILEGES ON DATABASE inventory_discrepancy         TO portfolio_user;
+GRANT ALL PRIVILEGES ON DATABASE sec_financial_intelligence    TO portfolio_user;
+GRANT ALL PRIVILEGES ON DATABASE jd_role_classifier            TO portfolio_user;
 
 -- Postgres 15+: GRANT ON DATABASE no longer implies schema public access
 \c compensation_ingest
@@ -20,4 +24,8 @@ GRANT ALL ON SCHEMA public TO portfolio_user;
 \c order_exception_agent
 GRANT ALL ON SCHEMA public TO portfolio_user;
 \c inventory_discrepancy
+GRANT ALL ON SCHEMA public TO portfolio_user;
+\c sec_financial_intelligence
+GRANT ALL ON SCHEMA public TO portfolio_user;
+\c jd_role_classifier
 GRANT ALL ON SCHEMA public TO portfolio_user;
